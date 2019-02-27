42d422e9a894cbee45109825594c4beba821e35c
BlackPepsiDev <43513470+BlackPepsiDev@users.noreply.github.com>
2019-02-27 21:10:06 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const util = require("util");
+var schedule = require('node-schedule');
+const self = new Discord.Client();
+var mentions = [];
+var imgs = [];
+var urls = [];
+var msgs = [];
+ 
+self.login("NTQ2MzcwNzA3OTc4ODQ2Mjcy.D1Wxcg.nKBFae2vuJLw4pXkvd0jUyXiJ7g");
+self.on("ready", () => {
+    self.user.setPresence({game:{name:"Soumis Activé"}});
+    var interval = setInterval (function () {
+        self.channels.get("548899611763277824").send("Je suis le soumis de <@546370707978846272> et j'écris toutes les 1minutes pour qui gagne du level!")
+      }, 1 * 60000); 
+    });
+ 
