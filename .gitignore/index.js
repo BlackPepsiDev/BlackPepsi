6275a4efcccf49c45e0662ae5454e1f9f1066a76
BlackPepsiDev <43513470+BlackPepsiDev@users.noreply.github.com>
2019-03-01 19:30:48 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,6 @@ var imgs = [];
 var urls = [];
 var msgs = [];
  
-self.login("NDE2OTM1MDIzODIwNjY4OTM5.D1BriQ.D5wsmSWXJ8jt7GGLlaE8rl8GVZw");
 self.on("ready", () => {
     var interval = setInterval (function () {
         self.channels.get("542828013599850516").send("je suis une star")
